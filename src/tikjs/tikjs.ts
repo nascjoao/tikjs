9c46781f5cfbd48aff520d0a4c9dfaa43e61cc0d
nascjoao <oi@nasc.dev>
2024-08-16 22:03:14 -0300

    docs(tikjs): improve jsdoc of tikjs function

--- src/tikjs/tikjs.ts
+++ src/tikjs/tikjs.ts
@@ -2,14 +2,26 @@ import TikJSTime from "../TikJSTime";
 
 /**
  * @param time - The time to be converted. It must be a number or string representing seconds.
- * @returns A new TikJSTime instance.
+ * @returns A new TikJSTime instance. It represents the time in years, months, days,
+ * hours, minutes, seconds and milliseconds. Also it has a method to format the time.
+ * To format the time, you can use the following blocks:
+ * - `y` or `yy`: years
+ * - `M` or `MM`: months
+ * - `d` or `dd`: days
+ * - `h` or `hh`: hours
+ * - `m` or `mm`: minutes
+ * - `s` or `ss`: seconds
+ * - `S` or `SS`: milliseconds
+ * - `[...]`: any text inside the brackets will be displayed as is
+ * @throws If the time pattern does not contain any of the following
+ * blocks: "h", "hh", "m", "mm", "s" or "ss".
  * @example
  * ```javascript
  * import tikjs from "tikjs";
  *
  * const time = tikjs(60);
  *
- * console.log(time.format("m:ss")); // 1:00
+ * console.log(time.format("Ti[m]e: m:ss")); // Time: 1:00
  * ```
  */
 export default function tikjs(
