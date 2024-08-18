153ef0b353d198a0e4719a83d010c8ec4ef5ff50
nascjoao <oi@nasc.dev>
2024-08-18 14:39:50 -0300

    test(TikJSTime): refactor tikjs tests

--- src/tikjs/tikjs.test.ts
+++ src/tikjs/tikjs.test.ts
@@ -64,31 +64,3 @@ describe("tikjs", () => {
         expect(time.milliseconds).toBe(600000);
     });
 });
-
-describe("tikjs.format", () => {
-    it("should return a formatted string", () => {
-        const time = tikjs(600);
-        const formattedTime = time.format("hh:mm:ss");
-        expect(formattedTime).toBe("00:10:00");
-    });
-
-    it("should throw an error if the format string is empty", () => {
-        const time = tikjs(600);
-        expect(() => time.format("")).toThrow(
-            'The time pattern "" must contain at least one of the following blocks: "h", "hh", "m", "mm", "s" or "ss".',
-        );
-    });
-
-    it("should throw an error if the format string has no blocks", () => {
-        const time = tikjs(600);
-        expect(() => time.format("abc")).toThrow(
-            'The time pattern "abc" must contain at least one of the following blocks: "h", "hh", "m", "mm", "s" or "ss".',
-        );
-    });
-
-    it("should escape blocks with square brackets", () => {
-        const time = tikjs(601);
-        const formattedTime = time.format("Ti[m]e: mm:ss");
-        expect(formattedTime).toBe("Time: 10:01");
-    });
-});
