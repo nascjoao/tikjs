a28cfcaa101225ffd4eb70e91e8fc221044fc7bd
nascjoao <oi@nasc.dev>
2024-08-20 17:28:43 -0300

    docs(tikjs): update to docs for new args

--- src/tikjs/tikjs.ts
+++ src/tikjs/tikjs.ts
@@ -2,9 +2,9 @@ import { type TikJSInput, TikJSTime } from "../TikJSTime";
 
 /**
  * **Tik.js** is a library to work with time in JavaScript. It allows you to easily
- * parse time to get durations, format them, and add or subtract time from dates.
+ * parse time to get durations and format them.
  *
- * @param time - The time to work with. It can be a number, a string or undefined.
+ * @param time - The time to work with. It can be a number or string.
  * If it's a number, it will be considered as a duration in seconds.
  * If it's a string, you can specify the unit of time by adding a suffix.
  * For example, "1d" for 1 day.
@@ -17,10 +17,6 @@ import { type TikJSInput, TikJSTime } from "../TikJSTime";
  * - `M` for months
  * - `y` for years
  *
- * If the time is undefined, it will return the TikJSTime class, which will give
- * you access to the dates methods.
- *
- * @returns The TikJSTime instance if the time is defined, or the TikJSTime class if it's not.
  * @example
  * ```javascript
  * const time = tikjs(3600); // 1 hour
@@ -31,10 +27,28 @@ import { type TikJSInput, TikJSTime } from "../TikJSTime";
  * ```
  * @example
  * ```javascript
- * const currentDate = new Date();
- * const newDate = tikjs().dates.add(currentDate, "1d");
- * console.log(newDate); // Adds 1 day to the current date.
+ * const time = tikjs('1h'); // 1 hour
+ * console.log(time.hours); // 1
+ * console.log(time.minutes); // 60
+ * console.log(time.seconds); // 3600
+ * console.log(time.format("h:mm:ss")); // 1:00:00
  * ```
+ *
+ * Also, you can get the duration between two dates:
+ *
+ * @param date1 - The first date.
+ * @param date2 - The second date.
+ *
+ * @example
+ * ```javascript
+ * const date1 = new Date('2021-01-01');
+ * const date2 = new Date('2021-01-02');
+ * const time = tikjs(date1, date2);
+ * console.log(time.days); // 1
+ * console.log(time.format("d [day]")) // 1 day
+ * ```
+ *
+ * @returns A TikJSTime object.
  */
 function tikjs(): typeof TikJSTime;
 function tikjs(time: TikJSInput): TikJSTime;
